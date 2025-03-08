58ebab8ca43c053a8025f624dd2c0334c98b8624
JPTheKnight <j.psaba_2002@hotmail.fr>
2025-03-08 19:23:15 +0100

    proper problem.py

--- problem.py
+++ problem.py
@@ -1,47 +1,43 @@
 import os
 import pandas as pd
 import rampwf as rw
-from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.model_selection import KFold
 
-problem_title = 'Predicting Legal Assistance Costs'
-_target_column_name = 'Montant total NDURINT'
+problem_title = "Predicting Legal Assistance Costs"
+_target_column_name = "Montant total NDURINT"
 _ignore_column_names = []
-_prediction_label_names = [0, 1]
 
-Predictions = rw.prediction_types.make_multiclass(
-    label_names=_prediction_label_names
-)
+Predictions = rw.prediction_types.make_regression()
 
-# An object implementing the workflow
 workflow = rw.workflows.Estimator()
 
-# The roc_auc score is the default metric for the challenge
 score_types = [
-    rw.score_types.ROCAUC(name='roc_auc', precision=4),
+    rw.score_types.RMSE(name="rmse", precision=4),
+    rw.score_types.MAE(name="mae", precision=4),
 ]
 
 
 def get_cv(X, y):
-    cv = StratifiedShuffleSplit(n_splits=8, test_size=0.2, random_state=42)
+    """Define cross-validation strategy (KFold)."""
+    cv = KFold(n_splits=5, shuffle=True, random_state=42)
     return cv.split(X, y)
 
 
-def csv_array_to_float(csv_array_string):
-    return list(map(float, csv_array_string[1:-1].split(',')))
-
-
 def _read_data(path, df_filename):
-    df = pd.read_csv(os.path.join(path, 'data', df_filename))
+    """Load dataset from CSV and split into features and target."""
+    df = pd.read_csv(os.path.join(path, "data", df_filename))
     y_array = df[_target_column_name].values
-    X_dict = df.drop(_target_column_name, axis=1)
+    X_dict = df.drop(columns=[_target_column_name])
     return X_dict, y_array
 
 
-def get_train_data(path='.'):
-    df_filename = 'train.csv'
+def get_train_data(path="."):
+    """Load training data."""
+    df_filename = "train.csv"
     return _read_data(path, df_filename)
 
 
-def get_test_data(path='.'):
-    df_filename = 'test.csv'
+def get_test_data(path="."):
+    """Load test data."""
+    df_filename = "test.csv"
     return _read_data(path, df_filename)
